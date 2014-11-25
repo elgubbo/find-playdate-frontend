d068420816c2016757f235d0e330c01c8bc73616
Alexander 'elgubbo' Reichert <a.reichert@webmelt.de>
2014-11-25 01:31:41 +0100

    changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -86,7 +86,7 @@ app.directive('errSrc', function() {
   return {
     link: function(scope, element, attrs) {
       element.bind('error', function() {
-        if (attrs.src != attrs.errSrc) {
+        if (attrs.src !== attrs.errSrc) {
           attrs.$set('src', attrs.errSrc);
         }
       });
