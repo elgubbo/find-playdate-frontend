1237d414c42ecde4e7da3aac92932d3bac419343
Alexander 'elgubbo' Reichert <a.reichert@webmelt.de>
2014-11-25 01:29:45 +0100

    changes

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -82,3 +82,14 @@ app.factory('PlayDate', function($resource) {
     });
 });
 
+app.directive('errSrc', function() {
+  return {
+    link: function(scope, element, attrs) {
+      element.bind('error', function() {
+        if (attrs.src != attrs.errSrc) {
+          attrs.$set('src', attrs.errSrc);
+        }
+      });
+    }
+  }
+});
