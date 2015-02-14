72fa65410cacf6ca06a076d064d9a9ed016cb29e
Alexander 'elgubbo' Reichert <a.reichert@webmelt.de>
2015-02-14 21:14:19 +0100

    changes to searchservice, made it more stateful

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,37 +1,32 @@
 'use strict';
 
 /**
- * @ngdoc function
- * @name htdocsApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the htdocsApp
- */
+* @ngdoc function
+* @name htdocsApp.controller:MainCtrl
+* @description
+* # MainCtrl
+* Controller of the htdocsApp
+*/
 angular.module('htdocsApp')
-  .controller('MainCtrl',['$scope', '$modal', '$routeParams', 'Search', 'PlayDate', 'FlashMessage',  function ($scope, $modal, $routeParams, Search, PlayDate, FlashMessage) {
-  	this.data = {
-      playdates: [],
-      flashMessages: []
-    };
+    .controller('MainCtrl',['$scope', '$modal', '$routeParams', 'Search', 'PlayDate', 'FlashMessage',  function ($scope, $modal, $routeParams, Search, PlayDate, FlashMessage) {
 
-    this.updateSearchData = function(data){
-      console.log('updatesearchdata called');
-      this.data.flashMessages = FlashMessage.getMessages();
-      this.data.playdates = data;
+    this.searchService = new Search();
+    this.data = {
+        flashMessages: []
     };
 
     this.openModal = function (playdate) {
 
       var modalInstance = $modal.open({
-        templateUrl: 'views/modal.html',
-        controller: 'ModalCtrl',
-        size: 'lg',
-        resolve: {
-          message: function () {
-            return {to: playdate.name, playdateId: playdate._id};
-          }
-        }
-      });
+            templateUrl: 'views/modal.html',
+            controller: 'ModalCtrl',
+            size: 'lg',
+            resolve: {
+                message : function () {
+                    return {to: playdate.name, playdateId: playdate._id};
+                }
+            }
+        });
 
       modalInstance.result.then(function (message) {
       });
@@ -39,48 +34,56 @@ angular.module('htdocsApp')
 
     this.openUpdateModal = function (playdate, mUpdateHash) {
 
-      var modalInstance = $modal.open({
-        templateUrl: 'views/updatemodal.html',
-        controller: 'UpdatemodalCtrl',
-        size: 'lg',
-        resolve: {
-          playdate: function () {
-            return playdate;
-          },
-          updateHash: function() {
-            return mUpdateHash;
-          }
-        }
-      });
+        var modalInstance = $modal.open({
+            templateUrl: 'views/updatemodal.html',
+            controller: 'UpdatemodalCtrl',
+            size: 'lg',
+            resolve: {
+                playdate: function () {
+                    return playdate;
+                },
+                updateHash: function() {
+                    return mUpdateHash;
+                }
+            }
+        });
 
-      modalInstance.result.then(function (message) {
-        if(message === 'success') {
-          Search.findPlayDates({});
-        }
-      });
+        modalInstance.result.then(function (message) {
+            if(message === 'success') {
+                this.searchService.findPlayDates({});
+            }
+        });
     };
 
-    this.receiveMore = function(data){
-      console.log('receive more called');
-      console.log(data);
-    };
-
-
-  	//self executing init function
-    (function() {
-        Search.registerObserverCallback(function(data){
-            $scope.main.updateSearchData(data);
-        });
-        Search.registerPushCallback(function(data){
-            $scope.main.receiveMore(data);
-        });
-        console.log('[INIT MAIN]');
+    $scope.$on('$viewContentLoaded', function() {
+        console.log('[WELCOME TO FIND-PLAYDATE.COM]');
+        console.log(this);
+        console.log($scope);
         if($routeParams.hash && $routeParams.id){
             PlayDate.getForUpdate({id: $routeParams.id, updateHash: $routeParams.hash}).$promise.then(function (playdate)
             {
               $scope.main.openUpdateModal(playdate, $routeParams.hash);
-            });
+          });
         }
-        Search.findPlayDates();
-    })();
+        $scope.main.searchService.findPlayDates();
+    });
+
+    // //self executing init function
+    // (function() {
+    //     console.log(this);
+    //     // $scope.main.searchService.registerObserverCallback(function(data){
+    //     //     $scope.main.updateSearchData(data);
+    //     // });
+    //     // $scope.main.searchService.registerPushCallback(function(data){
+    //     //     $scope.main.receiveMore(data);
+    //     // });
+    //     console.log('[INIT MAIN]');
+    //     if($routeParams.hash && $routeParams.id){
+    //         PlayDate.getForUpdate({id: $routeParams.id, updateHash: $routeParams.hash}).$promise.then(function (playdate)
+    //         {
+    //           $scope.main.openUpdateModal(playdate, $routeParams.hash);
+    //       });
+    //     }
+    //     $scope.main.searchService.findPlayDates();
+    // })();
 }]);
--- app/scripts/services/searchService.js
+++ app/scripts/services/searchService.js
@@ -2,87 +2,95 @@
 
 angular.module('htdocsApp')
 .factory('Search', function(PlayDate, FlashMessage) {
-    var search = {
-        observerCallbacks : [],
-        pushCallbacks : [],
-        results : [],
-        page : 1,
-        finishedLoading: false,
-        isLoading: false,
+    var Search = function(){
+        this.observerCallbacks = [];
+        this.pushCallbacks = [];
+        this.results = [];
+        this.page = 1;
+        this.finishedLoading = false;
+        this.isLoading = false;
+    };
 
-        findByPk : function(id)
-        {
-            var res = [];
-            angular.forEach(this.results, function(value) {
-                if (value.id === id) {
-                    this.push(value);
-                }
-            }, res);
-            return res;
-        },
 
-        //register an observer
-        registerObserverCallback : function(callback){
-            this.observerCallbacks.push(callback);
-        },
 
-        registerPushCallback: function(callback) {
-            this.pushCallbacks.push(callback);
-        },
-
-        pushObservers : function(data) {
-            angular.forEach(this.pushCallbacks, function(callback){
-                callback(data);
-            });
-        },
-
-        //call this when you know 'Search results have changed' has been changed
-        notifyObservers : function(data){
-            if (data.length === 0) {
-                FlashMessage.setMessage('warning', 'No PlayDates found, please try again.');
+    Search.prototype.findByPk = function(id)
+    {
+        var res = [];
+        angular.forEach(this.results, function(value) {
+            if (value.id === id) {
+                this.push(value);
             }
-            angular.forEach(this.observerCallbacks, function(callback){
-                callback(data);
-            });
-        },
+        }, res);
+        return res;
+    };
 
-        loadMore: function(){
-            if(!this.isLoading && !this.finishedLoading) {
-                var that = this;
-                this.page++;
-                this.data.page = this.page;
-                //return the this in case someone wants to handle the promise themselves
-                PlayDate.query(this.data).$promise.then(
-                    function(data){
-                        if(!data.length) {
-                            that.finishedLoading = true;
-                        }
-                        that.pushObservers(data);
-                    }
-                );
-            }
-        },
+    //register an observer
+    Search.prototype.registerObserverCallback = function(callback)
+    {
+        this.observerCallbacks.push(callback);
+    };
 
-        findPlayDates : function(data)
-        {
-            console.log(data);
-            if(data) {
-                this.data = data;
-            }
+    Search.prototype.registerPushCallback = function(callback)
+    {
+        this.pushCallbacks.push(callback);
+    };
+
+    Search.prototype.pushObservers = function(data)
+    {
+        angular.forEach(this.pushCallbacks, function(callback){
+            callback(data);
+        });
+    };
+
+    //call this when you know 'Search results have changed' has been changed
+    Search.prototype.notifyObservers = function(data)
+    {
+        if (data.length === 0) {
+            FlashMessage.setMessage('warning', 'No PlayDates found, please try again.');
+        }
+        angular.forEach(this.observerCallbacks, function(callback){
+            callback(data);
+        });
+    };
+
+    Search.prototype.loadMore = function()
+    {
+        if(!this.isLoading && !this.finishedLoading) {
             var that = this;
+            this.page++;
+            this.data.page = this.page;
             //return the this in case someone wants to handle the promise themselves
-            return PlayDate.query(data).$promise.then(
+            PlayDate.query(this.data).$promise.then(
                 function(data){
-                    that.notifyObservers(data);
-                    that.results = data;
-
-                },
-                function(error) {
-                    FlashMessage.setMessage('info', 'Unfortunately there has been an error, please try again later.');
-                    that.notifyObservers([]);
+                    if(!data.length) {
+                        that.finishedLoading = true;
+                    }
+                    that.pushObservers(data);
                 }
             );
-        },
+        }
     };
-    return search;
+
+    Search.prototype.findPlayDates = function(data)
+    {
+
+        if(data) {
+            this.data = data;
+        }
+        var that = this;
+        //return the this in case someone wants to handle the promise themselves
+        return PlayDate.query(data).$promise.then(
+            function(data){
+                that.notifyObservers(data);
+                that.results = data;
+
+            },
+            function(error) {
+                FlashMessage.setMessage('info', 'Unfortunately there has been an error, please try again later.');
+                that.notifyObservers([]);
+            }
+        );
+    };
+
+    return Search;
 });
