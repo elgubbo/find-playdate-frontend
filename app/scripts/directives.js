34fe2b9815bd2ee0ff3d9a067d210f36060ec41e
Alexander 'elgubbo' Reichert <elgubbo@gmail.com>
2015-07-31 15:33:14 +0200

    changed style of message modal

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -19,8 +19,8 @@ app.directive('errSrcModal', function() {
     return {
         link: function(scope, element, attrs) {
             element.bind('error', function() {
-                if (attrs.src !== attrs.errSrc) {
-                    attrs.$set('src', attrs.errSrc);
+                if (attrs.src !== attrs.errSrcModal) {
+                    attrs.$set('src', attrs.errSrcModal);
                     element.css('visibility', 'initial');
                 }
             });
