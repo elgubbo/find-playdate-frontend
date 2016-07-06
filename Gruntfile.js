2e793451f406e7171049ccdd4245ac926b47959c
Alexander 'elgubbo' Reichert <elgubbo@gmail.com>
2016-07-06 23:58:43 +0200

    changed gruntfile to include xml in build

--- Gruntfile.js
+++ Gruntfile.js
@@ -303,7 +303,7 @@ module.exports = function (grunt) {
           cwd: '<%= yeoman.app %>',
           dest: '<%= yeoman.dist %>',
           src: [
-            '*.{ico,png,txt}',
+            '*.{ico,png,txt,xml}',
             '.htaccess',
             '*.html',
             '*.json',
